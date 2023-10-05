96258c28f0c57b39e0ba1e6ca3b8c12dd589818c
Loomthie <mcfarldl@mail.uc.edu>
2023-10-05 11:31:58 -0400

    Updated to v 0.1.0

--- ChemPy/MER.py
+++ ChemPy/MER.py
@@ -4,52 +4,13 @@ import plotly.graph_objects as go
 
 class Therm_Stream:
 
-    def __init__(self,T_in,T_out,mCp):
+    def __init__(self,name,T_in,T_out,mCp):
+        self.Name = name
         self.T_in = T_in
         self.T_out = T_out
         self.mCp = mCp
         self.dTemp = self.T_out - self.T_in
         self.Enthalpy = abs(self.dTemp*self.mCp)
-
-
-# class MER:
-#
-#     def __init__(self,Chem_Heat:Therm_Stream,Chem_Cool:Therm_Stream,Min_T=10):
-#         self.Stream_H = Chem_Heat
-#         self.Stream_C = Chem_Cool
-#         self.minNetEnergy = abs(self.Stream_H.Enthalpy - self.Stream_C.Enthalpy)
-#         self.t_min = Min_T
-#         self.MER = self.__MER()
-#
-#     def __MER(self):
-#         adj_stream_C = Therm_Stream(
-#             self.Stream_C.T_in - self.t_min,
-#             self.Stream_C.T_out - self.t_min,
-#             self.Stream_C.mCp
-#         )
-#
-#         crit_Ts = [a.__dict__[key] for key in ['T_in','T_out'] for a in [adj_stream_C,self.Stream_H]]
-#         crit_Ts.sort(reverse=True)
-#         mCps = []
-#         for i in range(len(crit_Ts)-1):
-#             if adj_stream_C.T_in <= crit_Ts[i+1]:
-#                 mCps.append(-self.Stream_H.mCp)
-#             elif self.Stream_H.T_in > crit_Ts[i+1]:
-#                 mCps.append(adj_stream_C.mCp)
-#             else:
-#                 mCps.append(adj_stream_C.mCp-self.Stream_H.mCp)
-#
-#         dHs = [mCps[i]*(crit_Ts[i]-crit_Ts[i+1]) for i in range(len(mCps))]
-#         Rs = [0]
-#         for i in range(len(dHs)):
-#             Rs.append(Rs[len(Rs)-1]+dHs[i])
-#
-#         cols = ['Interval','T-hi','T-lo','mCp','dH','R']
-#         data = [
-#             [i+1,crit_Ts[i],crit_Ts[i+1],mCps[i],dHs[i],Rs[i+1]] for i in range(len(mCps))
-#         ]
-#
-#         return pd.DataFrame(data,columns=cols)
 
 
 class MER:
@@ -107,7 +68,7 @@ class MER:
         for s in self.heated_streams:
             fig.add_trace(
                 go.Scatter(
-                    x=2*[f'C{i}'],
+                    x=2*[s.Name],
                     y=[s.T_in,s.T_out],
                     line = dict(
                         color='#ff0000'
@@ -126,7 +87,7 @@ class MER:
         for s in self.adj_cooled_streams:
             fig.add_trace(
                 go.Scatter(
-                    x=2*[f'H{j}'],
+                    x=2*[s.Name],
                     y=[s.T_in,s.T_out],
                     line=dict(
                         color='#0000ff'
@@ -166,17 +127,3 @@ class MER:
 
         fig.show()
 
-
-# ben_stream = Therm_Stream(100,350,13.2e-3)
-# sty_stream = Therm_Stream(300,100,15.36e-3)
-#
-# res=MER(ben_stream,sty_stream)
-# print(res.res)
-
-c1_strm = Therm_Stream(120,235,20e-3)
-c2_strm = Therm_Stream(180,240,40e-3)
-h1_strm = Therm_Stream(260,160,30e-3)
-h2_strm = Therm_Stream(250,130,0.015)
-
-res = MER(c1_strm,c2_strm,h1_strm,h2_strm)
-print(res.res)
