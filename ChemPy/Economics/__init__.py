1641b8277a5eb643202aaca3c9f1ed34db07d4a3
Loomthie <mcfarldl@mail.uc.edu>
2023-10-11 00:22:15 -0400

    Updated to v 0.1.1

--- ChemPy/Economics/__init__.py
+++ ChemPy/Economics/__init__.py
@@ -1,22 +1,47 @@
 import pandas as pd
 import numpy as np
 
+
 class Currency:
 
     def __init__(self,value,unit='$',mode=0):
         self.value = value
         self.unit = unit
         self.mode = mode
-
     def __repr__(self):
         return f'{self.unit}{self.value/10**(3*self.mode):0.2f} {"".join(self.mode*["M"])}' \
     if self.value >= 0 else f'({self.unit}{self.value/10**(3*self.mode):0.2f}) {"".join(self.mode*["M"])}'
 
+    def __add__(self, other):
+        if isinstance(other,Currency):
+            if self.unit != other.unit:
+                raise ValueError()
+            return Currency(self.value+other.value)
+        return Currency(self.value+float(other),self.unit,self.mode)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        return Currency(self.value*other,self.unit,self.mode)
+
+    def __rmul__(self,other):
+        return self.__mul__(other)
 
 class Module:
 
-    def __init__(self):
-        pass
+    Lang_Cost = Currency(0)
+    IF_Cost = Currency(0)
+    Base_Cost = Currency(0)
+    CE_Target = 809
+    CE_Initial = 567
+
+    def __init__(self,Name:str,Desc:str):
+        self.Module_Name = Name
+        self.Module_Description = Desc
+        self.Updated_Lang_Cost = self.Lang_Cost*(self.CE_Target/self.CE_Initial)
+        self.Updated_IF_Cost = self.IF_Cost*(self.CE_Target/self.CE_Initial)
+        self.Updated_Base_Cost = self.Base_Cost*(self.CE_Target/self.CE_Initial)
 
 
 class HeatExchanger(Module):
@@ -26,6 +51,13 @@ class HeatExchanger(Module):
         12:1.12,
         16:1.05,
         20:1.00
+    }
+
+    fd_dict = {
+        'Fixed Head':0.85,
+        'Kettle Vaporizer':1.35,
+        'Floating Head':1.,
+        'U Tube':0.85
     }
 
     __cost_forms = {
@@ -39,15 +71,100 @@ class HeatExchanger(Module):
     def __fl_correlation(fl):
         return np.exp(0.35320481)*np.exp(-fl*0.01834923)
 
-    def __init__(self,pres,tube_length,area,exchanger_type='Fixed Head',Fm=1.):
+    def __init__(self,Name,Desc,pres,tube_length,area,exchanger_type='Fixed Head',Fm=1.,Fbm=3.17):
 
         self.Fp = 0.9803+.018*(pres/100)+.0017*(pres/100)**2
         self.Fl = self.fl_dict[tube_length] if tube_length in self.fl_dict \
             else self.__fl_correlation(tube_length)
-        self.baseCost = self.__cost_forms[exchanger_type](area)
+        baseCost = self.__cost_forms[exchanger_type](area)
+        self.Base_Cost = Currency(baseCost)
         self.Fm = Fm
+        self.Fbm = Fbm
+        self.Fd = self.fd_dict[exchanger_type] if exchanger_type in self.fd_dict else 1.
 
-        Lang_Cost = self.Fp*self.Fl*self.Fm*self.baseCost
+        Lang_Cost = self.Fp*self.Fl*self.Fm*baseCost
         self.Lang_Cost = Currency(Lang_Cost)
 
-        super().__init__()
+        IF_Cost = baseCost*((self.Fbm-1)/self.Fd+(self.Fm*self.Fp*self.Fl))
+        self.IF_Cost = Currency(IF_Cost)
+
+        super().__init__(Name,Desc)
+
+
+class Utilities:
+
+    def __init__(self,steam_flow=0,elec_pow=0,cool_water=0,proc_water=0,refrig=0):
+        self.steam = Currency(self.__steam_calc(steam_flow))
+        self.elec = Currency(self.__elec_calc(elec_pow))
+        self.cool_water = Currency(self.__cool_wat_calc(cool_water))
+        self.proc_water = Currency(self.__proc_wat_calc(proc_water))
+        self.refrigeration = Currency(self.__refridge_calc(refrig))
+        self.total_cost = self.steam+self.elec+self.cool_water+self.proc_water+self.refrigeration
+
+    @staticmethod
+    def __steam_calc(steam):
+        return 930*steam**.81
+
+    @staticmethod
+    def __elec_calc(s):
+        return 2.9e6*s**0.83
+
+    @staticmethod
+    def __cool_wat_calc(s):
+        return 1.1e3*s**0.68
+
+    @staticmethod
+    def __proc_wat_calc(s):
+        return 1.7e3*s**0.96
+
+    @staticmethod
+    def __refridge_calc(s):
+        return 12.5e3*s**0.77
+
+
+class Economic_Report:
+
+    class Operating_Costs:
+
+        __steam_costs = {
+            90:6.7
+        }
+
+        def __init__(self,Sales:Currency,Matl_Costs:Currency):
+            pass
+
+    def __init__(self,*Modules:Module,Sales:Currency|None=None,Util:Utilities | None = None, Project_Name='Economic Report',file_name:str | None = None):
+        self.modules = Modules
+        if not Util:
+            Util = Utilities()
+
+        if not Sales: Sales = Currency(0)
+
+        self.Salse=Sales
+
+        self.Utilities = Util
+        self.totalEscBMCost = Currency(np.sum([a.Updated_IF_Cost.value for a in self.modules]))
+        self.totalPurchaseCost = Currency(np.sum([a.Updated_Base_Cost.value for a in self.modules]))
+        self.instAndControls = self.totalPurchaseCost*.55
+        self.TBM_Cost = self.totalPurchaseCost+self.totalEscBMCost+self.instAndControls
+
+        self.sitePrep = 0.05*self.TBM_Cost
+        self.Service = 0.05*self.TBM_Cost
+
+        self.DPI = self.TBM_Cost+self.sitePrep+self.Service+Util.total_cost
+        self.contingency = 0.18*self.DPI
+        self.TDC = self.DPI+self.contingency
+
+        self.land = 0.02*self.TDC
+        self.royalties = 0.02*self.TDC
+        self.startUp = 0.1*self.TDC
+        self.TPI = self.TDC+self.land+self.royalties+self.startUp
+
+        self.WC = 0.15*self.TPI
+        self.TCI = self.TPI + self.WC
+
+
+e105 = HeatExchanger('E-105','',554,20,2250)
+test = Economic_Report(e105)
+
+
--- ChemPy/MER.py
+++ ChemPy/MER.py
@@ -29,7 +29,24 @@ class MER:
         self.heated_streams = [a for a in filter(lambda x: x.T_out>x.T_in,Streams)]
         self.cooled_streams = [a for a in filter(lambda x: x.T_in>x.T_out,Streams)]
         self.t_min = t_min
-        self.res = self.__MER()
+        self.df, self.crit_Ts = self.__MER()
+        self.pinch_temp = self.df[self.df.R == 0].T_lo.iloc[0]
+
+        cols = ['Streams','Temp High','Temp Low','mCp']
+        data = [
+            *[[a.Name,a.T_out,a.T_in,a.mCp] for a in self.heated_streams],
+            *[[a.Name,a.T_in-self.t_min,a.T_out-self.t_min,a.mCp] for a in self.cooled_streams]
+        ]
+
+        self.streams_df = pd.DataFrame(data,columns=cols)
+
+        cols = ['Streams','mCp','Q_below_pT','Q_above_pT']
+        data = [
+            *[[a.Name,a.mCp,a.mCp*(self.pinch_temp-a.T_in),a.mCp*(a.T_out-self.pinch_temp)] for a in self.heated_streams],
+            *[[a.Name,a.mCp,a.mCp*(self.pinch_temp-a.T_out+self.t_min),a.mCp*(a.T_in-self.t_min-self.pinch_temp)] for a in self.cooled_streams]
+        ]
+
+        self.Q_df = pd.DataFrame(data,columns=cols)
 
     def __MER(self):
         adj_c_strm = [Therm_Stream(a.Name,a.T_in - self.t_min,a.T_out-self.t_min,a.mCp) for a in self.cooled_streams]
@@ -40,8 +57,6 @@ class MER:
 
         crit_Ts = list(set(crit_Ts))
         crit_Ts.sort(reverse=True)
-
-        self.crit_Ts = crit_Ts
 
         columns = ['Interval','T_hi','T_lo','mCp','dH','R']
         data = [
@@ -69,9 +84,46 @@ class MER:
 
         df.R += max(-df.R)
 
-        return df
+        return df, crit_Ts
 
-    def show_diagram(self):
+    def generate_pinch_diagram(self):
+        fig = go.Figure()
+
+        streams = [*self.heated_streams,*self.cooled_streams]
+
+        for s in streams:
+            fig.add_trace(
+                go.Scatter(
+                    x=2 * [s.Name],
+                    y=[s.T_in, s.T_out],
+                    line=dict(
+                        color='#ff0000' if s in self.heated_streams else '#0000ff'
+                    ),
+                    marker=dict(
+                        size=10,
+                        symbol='arrow-bar-up',
+                        angleref='previous'
+                    )
+                )
+            )
+
+        fig.add_trace(
+            go.Scatter(
+                x=[streams[0].Name,streams[-1].Name],
+                y=2*[self.pinch_temp],
+                line=dict(
+                    dash='dash',
+                    color='#000'
+                ),
+                mode='lines'
+            )
+        )
+
+
+
+        return fig
+
+    def generate_hi_diagram(self):
         fig = go.Figure()
 
         i = 1
@@ -135,5 +187,16 @@ class MER:
             )
         )
 
-        fig.show()
+        return fig
 
+
+xyl = Therm_Stream('XYL',285,150,0.5039*50e-3)
+cyh = Therm_Stream('CYH',260,150,0.5693*44e-3)
+ben = Therm_Stream('BEN',140,250,0.4411*90e-3)
+tol = Therm_Stream('TOL',275,140,0.5021*46e-3)
+nhc = Therm_Stream('NHC',160,300,0.5777*50e-3)
+
+res = MER(xyl,cyh,ben,tol,nhc,t_min=20)
+
+res.generate_pinch_diagram().show()
+print(res.Q_df)
